76a1ea27f3078b224c74c9be953a108e4d60c30b
StudentAnanas <karoleg96@mail.ru>
2020-05-11 22:35:53 +0300

    button_run

---
+++ Lab7_button/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Lab7_button
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(24, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(436, 413);
+            this.panel1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Location = new System.Drawing.Point(159, 156);
+            this.button1.Margin = new System.Windows.Forms.Padding(5);
+            this.button1.MaximumSize = new System.Drawing.Size(200, 100);
+            this.button1.MinimumSize = new System.Drawing.Size(50, 25);
+            this.button1.Name = "button1";
+            this.button1.Padding = new System.Windows.Forms.Padding(5);
+            this.button1.Size = new System.Drawing.Size(118, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поймай меня";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AllowDrop = true;
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(480, 400);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(1);
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(484, 461);
+            this.tableLayoutPanel1.TabIndex = 2;
+            this.tableLayoutPanel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.tableLayoutPanel1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        public System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Lab7_button/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_button
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point cur = this.PointToClient(Cursor.Position);
+            Point loc = button1.Location;
+            int x_new = loc.X;
+            int y_new = loc.Y;
+
+            //Движение вправо
+            if (cur.X < loc.X + button1.Width)
+                x_new += 10;
+
+            //Движение влево
+            if (cur.X + 10 > loc.X + button1.Width)
+                x_new -= 10;
+
+            //Движение вверх
+            if (cur.Y + 10 > loc.Y + button1.Height)
+                y_new -= 10;
+
+            //Движение вниз
+            if (cur.Y < loc.Y + button1.Height)
+                y_new += 10;
+            
+            //Отступ слева
+            if (x_new < 5)
+                x_new = 5;
+
+            //Отступ справа
+            if (x_new > panel1.Width - button1.Width - 25)
+                x_new = panel1.Width - button1.Width - 25;
+
+            //Отступ сверху
+            if (y_new < 10)
+                y_new = 10;
+
+            //Отступ снизу
+            if (y_new > panel1.Height - button1.Height - 45)
+                y_new = panel1.Height - button1.Height - 45;
+
+            //курсор внизу формы
+            if (y_new > panel1.Height - button1.Height - 50)
+                y_new = panel1.Height - button1.Height - 80;
+
+            //курсор вверху формы
+            if (y_new < 11)
+                y_new += 50;
+
+            //курсор справа формы
+            if (x_new > panel1.Width - button1.Width - 26)
+                x_new += -100;
+
+            //курсор слева формы
+            if (x_new < 6)
+                x_new += 100;
+
+            button1.Location = new Point(x_new, y_new);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Поздравляем! Вы смогли нажать на кнопку!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Left = this.ClientSize.Width / 2 - button1.Width / 2;
+            button1.Top = this.ClientSize.Height / 2 - button1.Height / 2;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int x_new = button1.Location.X;
+            int y_new = button1.Location.X;
+            bool f = false;
+            //Отступ слева
+            if (x_new < 5)
+            {
+                f = true;
+                x_new = 5;
+            }
+
+
+            //Отступ справа
+            if (x_new > panel1.Width - button1.Width - 100) { x_new = panel1.Width - button1.Width - 25; f = true; }
+
+
+            //Отступ сверху
+            if (y_new < 10) { y_new = 10; f = true; }
+
+
+            //Отступ снизу
+            if (y_new > panel1.Height - button1.Height - 100) { y_new = panel1.Height - button1.Height - 45; f = true; }
+
+            if (f == true)
+                button1.Location = new Point(x_new, y_new);
+          
+        }
+
+        private void button1_ParentChanged(object sender, EventArgs e)
+        {
+            int x_new = button1.Location.X;
+            int y_new = button1.Location.X;
+            
+        }
+
+        private void tableLayoutPanel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+    }
+}
